0124686ff15353336b78697b9b8ae233a2fca2b1
alejobrand <santiagorenteria20071701@gmail.com>
2025-04-29 11:57:34 -0500

    fetvh pokemon y display pokemon

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
 const pokemonList = document.getElementById("pokemonList")
 
+async function fetchPokemonData(pokemonId){
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+        //"https://pokeapi.co/api/v2/pokemon/"+pokemonId
+         const data = await response.json()
+        console.log(data)
+        return data
+    } catch (error) {
+        console.error(error)   
+        return false
+    }
+}
+
+function displayPokemon(pokemon){
+    const pokemonCard = document.createElement("div")
+    pokemonCard.classList.add("pokemon-card")
+    console.log(pokemon.sprites.front_shiny)
+    pokemonCard.innerHTML = `
+    <h2>${pokemon.id}</h2>
+    <img src="${pokemon.sprites.front_shiny}"alt ="Imagen de ${pokemon.name}">
+       <h3> ${pokemon.name} </h3>
+    `
+    pokemonList.appendChild(pokemonCard)
+}
+
+async function loadPokedex(){
+    const pokemon =await fetchPokemonData(4)
+    console.log(pokemon)
+    displayPokemon(pokemon)
+    
+}
+
+loadPokedex()
